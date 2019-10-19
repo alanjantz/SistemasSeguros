8b23441c4a5b297d6ec19eb3f3e777a6cfca108c
Alan Jantz <kelphyz@gmail.com>
2019-10-18 21:58:47 -0300

    AES: Expansão de chave

---
+++ AdvancedEncryptionStandard/AdvancedEncryptionStandard/Exceptions/InvalidKeyException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedEncryptionStandard.Exceptions
+{
+    public class InvalidKeyException : Exception
+    {
+        public InvalidKeyException(string message, params object[] args)
+        {
+            throw new InvalidKeyException(string.Format(message, args));
+        }
+
+        public InvalidKeyException(string message) : base(message)
+        {
+        }
+    }
+}
--- AdvancedEncryptionStandard/AdvancedEncryptionStandard/Scrambler.cs
+++ AdvancedEncryptionStandard/AdvancedEncryptionStandard/Scrambler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdvancedEncryptionStandard.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -10,11 +11,12 @@ namespace AdvancedEncryptionStandard
     public class Scrambler : IDisposable, ICipher
     {
         private byte[] OriginalValue { get; set; }
-        private byte[] Key { get; set; }
+        private string Key { get; set; }
         private int KeySize { get; set; }
         private byte[] InitializationVector { get; set; }
-
         private CipherMode OperationMode { get; set; }
+        private PaddingMode Padding { get; set; }
+        private string[,] StateMatrix { get; set; }
 
         public Scrambler(string originalValue) : this(Encoding.ASCII.GetBytes(originalValue))
         {
@@ -24,15 +26,13 @@ namespace AdvancedEncryptionStandard
         {
             OriginalValue = originalValue;
             OperationMode = CipherMode.ECB;
+            Padding = PaddingMode.PKCS7; // Mesmo algoritmo que PKCS#5
             KeySize = 128;
+
+            StateMatrix = new string[4, 4];
         }
 
         public Scrambler WithKey(string key)
-        {
-            return this.WithKey(Encoding.ASCII.GetBytes(key));
-        }
-
-        public Scrambler WithKey(byte[] key)
         {
             this.Key = key;
 
@@ -48,7 +48,24 @@ namespace AdvancedEncryptionStandard
 
         public byte[] Encrypt()
         {
+            ExpandirChave();
+
             return null;
+        }
+
+        private void ExpandirChave()
+        {
+            InicializarMatrizEstado(this.Key, this.KeySize);
+        }
+
+        private void InicializarMatrizEstado(string key, int keySize)
+        {
+            if (key.Length != keySize / 8)
+                throw new InvalidKeyException("A chave \"{0}\" não condiz com o tamanho especificado ({1}).", key, keySize);
+
+            for (int line = 0, index = 0; line < 4; line++)
+                for (int column = 0; column < 4; column++, index++)
+                    StateMatrix[column, line] = key[index].ToHexByte();
         }
 
         ~Scrambler()
@@ -59,6 +76,21 @@ namespace AdvancedEncryptionStandard
         public void Dispose()
         {
             OriginalValue = null;
+        }
+    }
+
+    public static class Extentions
+    {
+        public static string ToHexByte(this char value)
+        {
+            try
+            {
+                return Convert.ToByte(value).ToString("x2");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
